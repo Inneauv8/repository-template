f8939b8372205ee162aedf3479767d8ca2932e13
Mathieu52 <Mathieu52@users.noreply.github.com>
2023-10-16 22:16:39 +0000

    deploy: 21c6dd94beff6b97229a4a9de9e4052c32e55119

--- menudata.js
+++ menudata.js
@@ -28,6 +28,4 @@ var menudata={children:[
 {text:"Liste des espaces de nommage",url:"namespaces.html"},
 {text:"Membres de l'espace de nommage",url:"namespacemembers.html",children:[
 {text:"Tout",url:"namespacemembers.html"},
-{text:"Fonctions",url:"namespacemembers_func.html"}]}]},
-{text:"Fichiers",url:"files.html",children:[
-{text:"Liste des fichiers",url:"files.html"}]}]}
+{text:"Fonctions",url:"namespacemembers_func.html"}]}]}]}
